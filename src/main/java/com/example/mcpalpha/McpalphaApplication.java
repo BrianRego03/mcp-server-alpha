3991c09cddff2756affb1b5baf7296e978a9ef05
Brian Dominic Rego <brian.rego03@gmail.com>
2026-03-06 18:00:46 +0530

    Create tool callback bean

--- src/main/java/com/example/mcpalpha/McpalphaApplication.java
+++ src/main/java/com/example/mcpalpha/McpalphaApplication.java
@@ -1,13 +1,25 @@
 package com.example.mcpalpha;
 
+import com.example.mcpalpha.services.toolService;
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class McpalphaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(McpalphaApplication.class, args);
+	}
+
+	@Bean
+	public List<ToolCallback> tools(toolService toolService) {
+		return List.of(ToolCallbacks.from(toolService));
 	}
 
 }
