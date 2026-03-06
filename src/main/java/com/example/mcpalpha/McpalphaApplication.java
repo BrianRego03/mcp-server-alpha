7e33d5767f05f427524d5fe38ab327ec5cd45f9c
Brian Dominic Rego <brian.rego03@gmail.com>
2026-03-06 17:08:45 +0530

    Add initial spring files

---
+++ src/main/java/com/example/mcpalpha/McpalphaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mcpalpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpalphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpalphaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mcpalpha/McpalphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mcpalpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpalphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
