242d28341832ff72854575d6ba390913a287c7c8
Brian Dominic Rego <brian.rego03@gmail.com>
2026-03-06 17:58:09 +0530

    Create methods to add and return items

--- src/main/java/com/example/mcpalpha/services/toolService.java
+++ src/main/java/com/example/mcpalpha/services/toolService.java
@@ -1,8 +1,10 @@
 package com.example.mcpalpha.services;
 
+import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,9 +13,21 @@ public class toolService {
     public record videoGame(String name, int price){
 
     }
-    private final Map<String,videoGame> map = new ConcurrentHashMap<>();
+    private final Map<String,videoGame> gameList = new ConcurrentHashMap<>();
 
-    
+    @Tool(name="addGame",
+        description = "Add a new game to the game list. Specify it's name and price")
+    public String addGame(String name, int price){
+        gameList.put(name,new videoGame(name, price));
+
+        return "New Game Added: '" + name + "' at price: " + price;
+    }
+
+    @Tool(name = "getGameList",
+        description = "Get all games currently in game list")
+    public List<videoGame> getGameList(){
+        return new ArrayList<>(gameList.values());
+    }
 
 
 }
